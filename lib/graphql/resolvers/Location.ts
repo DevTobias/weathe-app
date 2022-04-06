71067999c762e3697844920182de770f4a49769e
DevTobias <tobi.kaerst@gmx.de>
2022-04-06 12:42:53 +0200

    feat: added details resolver for locations

--- lib/graphql/resolvers/Location.ts
+++ lib/graphql/resolvers/Location.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { DateTime } from 'luxon';
 import { Context } from '../context';
 
 /**
@@ -11,4 +13,33 @@ export const resolveLocations = async ({ prisma }: Context) => {
   return prisma.locations.findMany({
     orderBy: [{ locationName: 'desc' }],
   });
+};
+
+/**
+ * Get the details from the open weather api from a provided name.
+ *
+ * @param name  The name of the location.
+ * @returns     The weather details for this location.
+ */
+export const resolveDetailsFromName = async (name: string) => {
+  // Remove umlauts from the name so the request don't get corrupted
+  const cleanName = name.toLowerCase().replace('ö', 'oe').replace('ä', 'ae').replace('ü', 'ue');
+
+  const { data } = await axios({
+    method: 'get',
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${cleanName}&appid=${process.env.WEATHER_KEY}&units=metric&lang=de`,
+  });
+
+  // Parse the data into a valid format
+  const { weather, main } = data;
+  const date = DateTime.local(DateTime.now()).setZone('UTC').toFormat('EEEE, D');
+  const temperature = `${Math.round(main.temp_min)}°C / ${Math.round(main.temp_max)}°C`;
+
+  return {
+    icon: weather[0].icon,
+    date,
+    temperature,
+    description: weather[0].description,
+    probabilityRain: main.humidity,
+  };
 };
--- lib/graphql/types/Location.ts
+++ lib/graphql/types/Location.ts
@@ -1,5 +1,20 @@
 import { extendType, nonNull, objectType } from 'nexus';
-import { resolveLocations } from '../resolvers/Location';
+import { resolveLocations, resolveDetailsFromName } from '../resolvers/Location';
+
+/**
+ * Describes the API Endpoint for details of a specific
+ * weather location.
+ */
+export const LocationDetails = objectType({
+  name: 'LocationDetails',
+  definition(t) {
+    t.string('icon');
+    t.string('date');
+    t.string('temperature');
+    t.string('description');
+    t.float('probabilityRain');
+  },
+});
 
 /**
  * Describes the API Endpoint for location data.
@@ -9,6 +24,10 @@ export const Location = objectType({
   definition(t) {
     t.nonNull.id('id');
     t.nonNull.string('locationName');
+    t.nonNull.field('details', {
+      type: LocationDetails,
+      resolve: async ({ locationName }) => resolveDetailsFromName(locationName),
+    });
   },
 });
 
