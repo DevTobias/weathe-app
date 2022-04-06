ba49e52b7f606e7ad97207d6005f1bc6576d1f7b
DevTobias <tobi.kaerst@gmx.de>
2022-04-06 09:41:07 +0200

    chore: configured tailwind and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['pages/**/*.{js,jsx,ts,tsx}', 'app/Components/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      height: {
+        innerScreen: 'calc(var(--innerVh, 1vh) * 100)',
+      },
+      minHeight: {
+        innerScreen: 'calc(var(--innerVh, 1vh) * 100)',
+      },
+      colors: {
+        transparent: '#00000000',
+        neutral: {
+          900: '#001427',
+          800: '#1a2c3d',
+          700: '#334352',
+          600: '#4d5b68',
+          500: '#66727d',
+          400: '#808a93',
+          300: '#99a1a9',
+          200: '#b3b9be',
+          150: '#ccd0d4',
+          100: '#e6e8e9',
+          0: '#FFFFFF',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
